58fd9c6c2e5a61a292667a4a9b4bde9514f7c812
ashneu <31425942+ashneu@users.noreply.github.com>
2018-01-04 23:27:06 -0600

    inital calculator box

---
+++ class-work/calculator-box/js/script.js
@@ -0,0 +1,21 @@
+
+
+function addToOut (){
+ $('#out').html('10');
+}
+
+$('#a10').click(addToOut)
+
+function addMoreToOut (){
+	$('#out').html('30');
+}
+
+$('#a20').click(addMoreToOut);
+
+function addBlue() {
+	$('#out').val
+}
+
+function addToOut (){
+	$('#out')
+}
