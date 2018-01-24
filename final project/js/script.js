7c6f067256bf9710e81e24e9b6f360e2d8f09213
ashneu <31425942+ashneu@users.noreply.github.com>
2018-01-23 21:18:06 -0600

    js first draft notes

--- final project/js/script.js
+++ final project/js/script.js
@@ -1,1 +1,3 @@
-script.js
+/* when click on first piece of trash, it spins and fades away and the fact below 
+it becomes bigger and takes more of that whole space....same with the 2nd picture..then when the last picture is
+clicked, and about 10 seconds of the the fact popping up, it takes you to the next html page */
