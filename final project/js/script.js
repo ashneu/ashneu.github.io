1342c6ab1392303b8606186622d9efa766be8340
ashneu <31425942+ashneu@users.noreply.github.com>
2018-01-28 18:58:38 -0600

    js part 2

--- final project/js/script.js
+++ final project/js/script.js
@@ -1,3 +1,36 @@
-/* when click on first piece of trash, it spins and fades away and the fact below 
-it becomes bigger and takes more of that whole space....same with the 2nd picture..then when the last picture is
-clicked, and about 10 seconds of the the fact popping up, it takes you to the next html page */
+/* 1)the trash fades out like i want but when the facts fade in, i'm not sure how to replace them in the same spot 
+as the trash..they currently show up below and you have to scroll. I tried the replaceWith and the fadeout fadein and can't get it to work properly.
+2)Also the animate css for the word 'waste' on 2nd html page doesn't work properly like the website shows 
+3)to get to 2nd html page, click the word waste after click all trash
+*/
+
+
+
+/* this is what i thought would work but didn't
+
+
+$('.cigpic').click(function () {
+	$('.cigpic').replaceWith($('.cigarette'));
+});
+
+*/
+
+$('.cigpic').click(function () {
+	$('.cigpic').fadeOut('slow', function(){
+        $('.cigarette').fadeIn('slow');
+    });
+});
+
+$('.plasticpic').click(function () {
+	$('.plasticpic').fadeOut('slow', function(){
+        $('.plastic').fadeIn('slow');
+    });
+});
+
+$('.foampic').click(function () {
+	$('.foampic').fadeOut('slow');
+	$('.foam').fadeIn('slow');
+	
+});
+
+
